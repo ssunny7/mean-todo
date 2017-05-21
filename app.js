a1ef2664417cb59cc39e708e80712a1d0c8dc18b
Sunny Shah <shahsunny712@gmail.com>
2017-05-21 15:49:40 -0500

    Added routers to the controller for adding a new todo and getting all todos. Also updated the app config to use the controller for the /todos route.

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+var TodoController = require('./todos/TodoController');
+app.use('/todos', TodoController);
+
 module.exports = app;
--- todos/TodoController.js
+++ todos/TodoController.js
@@ -5,4 +5,25 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 var todo = require('./todo_schema');
 
+router.post('/', function(req, res) {
+   TodoSchema.create({
+       title: req.body.title,
+       description: req.body.description,
+       completed: req.body.completed
+   },
+   function(err, todo) {
+       if(err)
+           return res.status(500).send("The todo could not be added to the database!");
+       return res.status(200).send(todo);
+   });
+});
+
+router.get('/', function(req, res) {
+   TodoSchema.find({}, function(err, todos) {
+       if(err)
+           return res.status(500).send("The todos could not be retrieved from the database!");
+       return res.status(200).send(todos);
+   });
+});
+
 module.exports = router;
