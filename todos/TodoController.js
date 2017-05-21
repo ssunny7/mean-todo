1443c7d52fc952fa41ba34269bc41f80ddccd73f
Sunny Shah <shahsunny712@gmail.com>
2017-05-21 16:13:40 -0500

    Added routers for get by ID, deletion and update of todos.

--- todos/TodoController.js
+++ todos/TodoController.js
@@ -6,7 +6,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 var todo = require('./todo_schema');
 
 router.post('/', function(req, res) {
-   TodoSchema.create({
+   todo.create({
        title: req.body.title,
        description: req.body.description,
        completed: req.body.completed
@@ -19,10 +19,34 @@ router.post('/', function(req, res) {
 });
 
 router.get('/', function(req, res) {
-   TodoSchema.find({}, function(err, todos) {
+   todo.find({}, function(err, todos) {
        if(err)
            return res.status(500).send("The todos could not be retrieved from the database!");
        return res.status(200).send(todos);
+   });
+});
+
+router.get('/:id', function(req, res) {
+   todo.findById(req.params.id, function(err, todo) {
+       if(err)
+           return res.status(500).send("The requested todo was not found in the database!");
+       return res.status(200).send(todo);
+   });
+});
+
+router.delete('/:id', function(req, res) {
+   todo.findByIdAndRemove(req.params.id, function(err, todo) {
+       if(err)
+           return res.status(500).send("The requested todo could not be deleted from the database!");
+       return res.status(200).send("The todo titled " + todo.title + " was deleted!");
+   });
+});
+
+router.put('/:id', function(req, res) {
+   todo.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, todo) {
+       if(err)
+           return res.status(500).send("The requested todo could not be updated!");
+       return res.status(200).send(todo);
    });
 });
 
