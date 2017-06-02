b13827e9aae28abacb1921c40c3239df6e322514
Sunny Shah <shahsunny712@gmail.com>
2017-06-01 23:18:10 -0500

    Added comments to the backend and frontend app files

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var morgan = require('morgan');
-var db = require('./db');
-var bodyParser = require('body-parser');
+/* Get reference to express module */
+var express = require('express'); // Get express module
+var app = express(); // This treats express as a function -- it is both a module and a function
 
+/* Get reference to path for use later */
+var path = require('path');
+
+/* morgan enables logging of messages about requests */
+var morgan = require('morgan');
+
+/* Get reference to the database module */
+var db = require('./db');
+
+/* This should be set to enable serving of static content (like for an SPA). Should contain the bower_components, app.js and html files */
 app.use(express.static(__dirname + '/client'));
+
+/* Other required statements */
+var bodyParser = require('body-parser');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended' : 'true'}));
 
+/* Set router for the backend */
 require('./backend/todo_rest')(app);
 
 module.exports = app;
