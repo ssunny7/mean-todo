f420c146c678db57dab50bbe49f1204166de7c28
Sunny Shah <shahsunny712@gmail.com>
2017-06-01 23:48:32 -0500

    Updated look and feel of main page using bootstrap

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+/* Get reference to main module defined in app.js */
 var app = require('./app');
+
+/* Set port */
 var port = process.env.PORT || 3000;
 
+/* Start server to listen on set port */
 var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + port);
 });
