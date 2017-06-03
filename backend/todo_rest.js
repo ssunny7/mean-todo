44a144d9515d8b075b3f1ede02e639b72af807da
Sunny Shah <shahsunny712@gmail.com>
2017-06-02 20:53:33 -0500

    Changes to support todo deletion
    Main page now includes a readonly checkbox showing whether or not the todo
    is completed as well as a button to delete the todo.
    
    Angular controller contains methods to create and delete a todo.
    
    Backend is updated to retrieve all todos remaining after deleting one.

--- backend/todo_rest.js
+++ backend/todo_rest.js
@@ -9,9 +9,10 @@ module.exports = function(_app) {
                 completed: req.body.completed
             },
             function(err, todo) {
+                console.log('API POST; data: ' + todo);
                 if(err)
                     return res.status(500).send("The todo could not be added to the database!");
-                return res.status(200).json(todo);
+                return res.json(todo);
             });
     });
 
@@ -26,25 +27,34 @@ module.exports = function(_app) {
 
     _app.get('/api/todos/:id', function(req, res) {
         TodoSchema.findById(req.params.id, function(err, todo) {
+            console.log('API GET id; length: ' + todo.length + ', data: ' + todo);
             if(err)
                 return res.status(500).send("The requested todo was not found in the database!");
-            return res.status(200).json(todo);
+            return res.json(todo);
         });
     });
 
     _app.delete('/api/todos/:id', function(req, res) {
         TodoSchema.findByIdAndRemove(req.params.id, function(err, todo) {
+            console.log('API DELETE; data: ' + todo);
             if(err)
                 return res.status(500).send("The requested todo could not be deleted from the database!");
-            return res.status(200).json("The todo titled " + todo.title + " was deleted!");
+
+            TodoSchema.find({}, function(err, todos) {
+                console.log('API GET after DELETE; data: ' + todos);
+                if(err)
+                    return res.status(500).send("The todos after deletion could not be retrieved from the database!");
+                return res.json(todos);
+            });
         });
     });
 
     _app.put('/api/todos/:id', function(req, res) {
         TodoSchema.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, todo) {
+            console.log('API PUT; data: ' + todo);
             if(err)
                 return res.status(500).send("The requested todo could not be updated!");
-            return res.status(200).json(todo);
+            return res.json(todo);
         });
     });
 
--- client/app.js
+++ client/app.js
@@ -2,14 +2,41 @@ var app = angular.module('todosApp', []);
 
 app.controller('todosController', function($scope, $http) {
     $scope.todos = {};
+    $scope.newData = {};
 
     $http({
         method: 'GET',
         url: '/api/todos'
     }).then(function (success) {
-        console.log(success.data);
+        console.log('Angular GET success: ' + success.data);
         $scope.todos = success.data;
     }, function (error) {
         console.log('Angular GET error: ' + error.data);
     });
+
+    $scope.createToDo = function() {
+        $http({
+            method: 'POST',
+            url: '/api/todos',
+            data: $scope.newData
+        }).then(function (success) {
+            console.log('Angular POST success: ' + success.data);
+            $scope.newData = {};
+            $scope.todos = success.data;
+        }, function (error) {
+            console.log('Angular POST error: ' + error.data);
+        });
+    };
+
+    $scope.deleteToDo = function(_id) {
+        $http({
+            method: 'DELETE',
+            url: '/api/todos/' + _id
+        }).then(function (success) {
+            console.log('Angular DELETE success: ' + success.data);
+            $scope.todos = success.data;
+        }, function (error) {
+            console.log('Angular DELETE error: ' + error.data);
+        });
+    };
 });
