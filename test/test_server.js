5043b45e799664ffc678f7e102ea8c26085eb67b
Sunny Shah <shahsunny712@gmail.com>
2017-06-07 01:04:47 -0500

    Added test case for the GET endpoint

--- test/test_server.js
+++ test/test_server.js
@@ -6,7 +6,17 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('todos', function() {
-    it('should list all todos via GET at /api/todos');
+    it('should list all todos via GET at /api/todos', function(done) {
+        chai.request(server)
+            .get('/api/todos')
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                done();
+            });
+    });
+
     it('should list a single todo via GET at /api/todos/:id');
     it('should add a single todo via POST at /api/todos');
     it('should delete a single todo via DELETE at /api/todos/:id');
