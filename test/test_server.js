83c6936bdd43335dce310364207fa8557cd91a1b
Sunny Shah <shahsunny712@gmail.com>
2017-06-07 18:47:54 -0500

    Added test case for POST
    This also adds dependency on chai-things
    (https://github.com/chaijs/chai-things) to test array element values.

--- test/test_server.js
+++ test/test_server.js
@@ -2,8 +2,10 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../app');
 var should = chai.should();
+var chaiThings = require('chai-things');
 
 chai.use(chaiHttp);
+chai.use(chaiThings);
 
 describe('todos', function() {
     it('should list all todos via GET at /api/todos', function(done) {
@@ -18,7 +20,24 @@ describe('todos', function() {
     });
 
     it('should list a single todo via GET at /api/todos/:id');
-    it('should add a single todo via POST at /api/todos');
+
+    it('should add a single todo via POST at /api/todos', function(done) {
+        var curTodoNum = Math.floor(Math.random() * 1000) + 1;
+
+        chai.request(server)
+            .post('/api/todos')
+            .send({'title':'Test ' + curTodoNum,'description':'Test todo ' + curTodoNum,'completed':'false'})
+            .end(function (err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.should.contain.an.item.with.property('title', 'Test ' + curTodoNum);
+                res.body.should.contain.an.item.with.property('description', 'Test todo ' + curTodoNum);
+                res.body.should.contain.an.item.with.property('completed', false);
+                done();
+            });
+    });
+
     it('should delete a single todo via DELETE at /api/todos/:id');
     it('should update a single todo via PUT at /api/todos/:id');
 });
