2baea773615b9faf50d1e93ea2bf991e234da0cd
Sunny Shah <shahsunny712@gmail.com>
2017-06-06 19:11:18 -0500

    Added boilerplate for server tests
    Added file with skeleton code to test the server. Tests will use mocha and
    chai.

---
+++ tests/test_server.js
@@ -0,0 +1,7 @@
+describe('todos', function() {
+    it('should list all todos via GET at /api/todos');
+    it('should list a single todo via GET at /api/todos/:id');
+    it('should add a single todo via POST at /api/todos');
+    it('should delete a single todo via DELETE at /api/todos/:id');
+    it('should update a single todo via PUT at /api/todos/:id');
+});
