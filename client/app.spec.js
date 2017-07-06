8d4a8ebb0e6ee1ddc9927f2a9b9779cb5e0d2151
Sunny Shah <shahsunny712@gmail.com>
2017-07-05 21:25:46 -0500

    Added angular test case for GET requests.
    Uses one mock JSON object and verifies its receipt.

--- client/app.spec.js
+++ client/app.spec.js
@@ -1,24 +1,31 @@
 describe('Default Test Suite - todos', function() {
-    var $httpBackend, scope, controller;
+    var httpBackend, scope, controller;
 
     beforeEach(module('todosApp'));
 
     beforeEach(inject(function($injector) {
-        $httpBackend = $injector.get('$httpBackend');
-        scope = $injector.get('$rootScope');
-
-        var $controller = $injector.get('$controller');
-        controller = $controller('todosController', {$scope: scope});
+        httpBackend = $injector.get('$httpBackend');
+        scope = $injector.get('$rootScope').$new();
+        controller = $injector.get('$controller');
     }));
 
     afterEach(function() {
-        $httpBackend.verifyNoOutstandingExpectation();
-        $httpBackend.verifyNoOutstandingRequest();
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
     });
 
     it('should have an instance of the todos array', function() {
+        var ctrl = controller('todosController', {$scope: scope});
+
         expect(scope.todos).toBeDefined();
         expect(scope.toEdit).toBeDefined();
         expect(scope.newData).toBeDefined();
+    });
+
+    it('should fetch todos', function() {
+        httpBackend.when('GET', '/api/todos').respond(200, {'title':'Mock Todo 1', 'description':'Todo 1 for mocking',  'completed': true});
+        var ctrl = controller('todosController', {$scope: scope});
+        httpBackend.flush();
+        expect(scope.todos).toEqual({'title':'Mock Todo 1', 'description':'Todo 1 for mocking',  'completed': true});
     });
 });
